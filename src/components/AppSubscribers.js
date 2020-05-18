cf5e05865ed063ab81a95281e7072dcfa9f3a46e
lauti7 <laugerbustos@hotmail.com>
2020-05-17 23:28:51 -0300

    added SubscribersTable component

--- src/components/AppSubscribers.js
+++ src/components/AppSubscribers.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react'
 import {Container, Row, Col, Table} from 'reactstrap'
 import {useAuth} from '../auth'
 import moment from 'moment'
+import SubscribersTable from './SubscribersTable'
 
 
 const AppSubscribers = (props) => {
@@ -40,43 +41,7 @@ const AppSubscribers = (props) => {
           <h1 className="mt-3">Subscribers</h1>
         </Col>
       </Row>
-      <Row>
-        <Col md={10}>
-          {
-            subscribers.length > 0 ?
-              <Table style={{backgroundColor: 'rgb(255, 255, 255)', boxShadow: '-3px 3px 1px 1px rgba(0,0,0,0.05)'}} bordered>
-                <thead>
-                  <tr>
-                    <th>#</th>
-                    <th>Opt-In Date</th>
-                    <th>Device</th>
-                    <th>Language</th>
-                    <th>Country</th>
-                    <th>Sessions Count</th>
-                    <th>Last Time Active</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {
-                    subscribers.map((sub, idx) =>
-                      <tr key={sub._id}>
-                        <td>{idx + 1}</td>
-                        <td>{moment(sub.optInDate).format("dddd, MMMM Do YYYY")}</td>
-                        <td>{sub.os}</td>
-                        <td>{sub.lang}</td>
-                        <td>{sub.country}</td>
-                        <td>{sub.sessionsCount}</td>
-                        <td>{moment(sub.lastSession).fromNow()}</td>
-                      </tr>
-                    )
-                  }
-                </tbody>
-              </Table>
-              :
-                <p>There is no subscriber yet :(</p>
-          }
-        </Col>
-      </Row>
+      <SubscribersTable subscribers={subscribers}/>
     </Container>
   )
 }
